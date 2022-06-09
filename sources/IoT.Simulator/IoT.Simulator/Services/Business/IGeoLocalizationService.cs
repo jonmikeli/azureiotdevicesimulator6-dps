00db3047b33757458d2c5c1bfb71dd82f349b1b0
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2022-06-09 12:02:55 +0200

    new service contract

---
+++ sources/IoT.Simulator/IoT.Simulator/Services/Business/IGeoLocalizationService.cs
@@ -0,0 +1,9 @@
+﻿using IoT.Simulator.Models;
+
+namespace IoT.Simulator.Services.Business
+{
+    public interface IGeoLocalizationService
+    {
+        public Position RandomizePosition(Position initialPosition, PositionRandomType randomType, RandomPositionPrecision precision);
+    }
+}
