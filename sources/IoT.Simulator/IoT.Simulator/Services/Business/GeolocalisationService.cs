e54baf5b2dfd14d87aafe47c2fc799ff0f182033
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2022-06-09 12:03:21 +0200

    new service implementation

---
+++ sources/IoT.Simulator/IoT.Simulator/Services/Business/GeolocalisationService.cs
@@ -0,0 +1,24 @@
+﻿using IoT.Simulator.Models;
+
+namespace IoT.Simulator.Services.Business
+{
+    public class GeolocalisationService: IGeoLocalizationService
+    {
+        public Position RandomizePosition(Position initialPosition, PositionRandomType randomType, RandomPositionPrecision precision)
+        {
+            Position result = initialPosition;
+
+            switch (randomType)
+            {
+                case PositionRandomType.Forward:
+                    break;
+                case PositionRandomType.FullRandom:
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
